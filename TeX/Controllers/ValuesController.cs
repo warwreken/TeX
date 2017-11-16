cd8473c7d45cc2d3182f41feae868780761476f0
Gabriel Moreira de Oliveira <warwreken@gmail.com>
2017-11-16 14:46:57 -0200

    Initial commit

---
+++ TeX/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using TeX.Models;
+
+namespace TeX.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public WebHook Post([FromBody]string value)
+        {
+            var wh = new WebHook();
+
+            wh.Speech = "Funcionou o Speech";
+            wh.DisplayText = "Funcionou o Display";
+            wh.Source = "WebHookSource";
+
+            return wh;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TeX/Models/WebHook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeX.Models
+{
+    public class WebHook
+    {
+        public string Speech { get; set; }
+        public string ContextOut { get; set; }
+        public string Data { get; set; }
+        public string DisplayText { get; set; }
+        public string Source { get; set; }
+    }
+}
